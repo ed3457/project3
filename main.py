93645bf36bf9947c8b56315f398f42c2a509bb18
Kamel R <kamel.r@wayne.edu>
2022-05-30 23:21:52 -0400

    Port changed

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
+@app.route('/joke/', methods=['GET', 'POST'])
+def joke():
+    return "Hello World!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
